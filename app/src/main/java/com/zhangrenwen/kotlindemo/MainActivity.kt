00da8bf2d69253d5b28e066c590f89c1c031d385
GoodWen <479696877@qq.com>
2020-12-07 17:10:47 +0800

    kotlin 动态权限申请

--- app/src/main/java/com/zhangrenwen/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/zhangrenwen/kotlindemo/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.zhangrenwen.kotlindemo
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), 1)
+        } else {
+            Toast.makeText(this, "拥有权限正常执行", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    Toast.makeText(this, "权限请求成功回调", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "没有权限", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
